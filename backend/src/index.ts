43d80def24e795e2d1090d12b4c08433969e08fb
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2024-08-26 07:40:03 +0530

    add endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,11 +6,11 @@ app.get('/', (c) => {
 	return c.text('Hello Hono!');
 });
 
-app.post('api/v1/signup', (c) => c.text('POST /'));
-app.post('api/v1/signup', (c) => c.text('POST /'));
-app.post('api/v1/blog', (c) => c.text('POST /'));
-app.put('api/v1/blog', (c) => c.text('POST /'));
-app.get('api/v1/blog/:id', (c) => c.text('POST /'));
-app.get('api/v1/blog/bulk', (c) => c.text('POST /'));
+app.post('api/v1/signup', (c) => c.text('sign up /'));
+app.post('api/v1/signin', (c) => c.text('sign in /'));
+app.post('api/v1/blog', (c) => c.text('blog post /'));
+app.put('api/v1/blog', (c) => c.text('blog put /'));
+app.get('api/v1/blog/:id', (c) => c.text('blog id /'));
+app.get('api/v1/blog/bulk', (c) => c.text('blog get /'));
 
 export default app;
