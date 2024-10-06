18200573e48d1912df455d234ba03fec1c33eb75
saadahmadhere <saadahmad728@gmail.com>
2024-10-06 15:02:01 +0530

    [feat]: add signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,37 @@
 import { Hono } from 'hono';
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-const app = new Hono();
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string; // to give a type to the environment variable, otherwise typsecript will complain.
+	};
+}>();
 
 app.get('/', (c) => {
-	return c.text('Hello Hono!');
+	return c.text('Hello Hono  adsf!');
 });
 
-app.post('api/v1/signup', (c) => c.text('sign up /'));
+app.post('api/v1/signup', async (c) => {
+	const dbUrl = c.env.DATABASE_URL;
+	const prisma = new PrismaClient({
+		datasourceUrl: dbUrl,
+	}).$extends(withAccelerate());
+
+	// console.log('db url', dbUrl);
+	const body = await c.req.json();
+
+	await prisma.user.create({
+		data: {
+			email: body.email,
+			password: body.password,
+			firstName: body.firstName,
+		},
+	});
+
+	console.log('body,', body);
+	return c.text('sign up /');
+});
 app.post('api/v1/signin', (c) => c.text('sign in /'));
 app.post('api/v1/blog', (c) => c.text('blog post /'));
 app.put('api/v1/blog', (c) => c.text('blog put /'));
