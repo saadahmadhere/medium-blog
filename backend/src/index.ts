6c0601f0dc44b1f869129319bde82202710775e1
saadahmadhere <saadahmad728@gmail.com>
2024-10-06 18:28:01 +0530

    feat: add singin route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -37,7 +37,28 @@ app.post('api/v1/signup', async (c) => {
 		token,
 	});
 });
-app.post('api/v1/signin', (c) => c.text('sign in /'));
+
+app.post('api/v1/signin', async (c) => {
+	const dbUrl = c.env.DATABASE_URL;
+	const prisma = new PrismaClient({
+		datasourceUrl: dbUrl,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+
+	const user = await prisma.user.findUnique({
+		where: {
+			email: body.email,
+		},
+	});
+
+	if (!user) {
+		return c.json({ message: 'user not found' });
+	}
+
+	const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+	return c.json({ message: 'login succesfully', token });
+});
 app.post('api/v1/blog', (c) => c.text('blog post /'));
 app.put('api/v1/blog', (c) => c.text('blog put /'));
 app.get('api/v1/blog/:id', (c) => c.text('blog id /'));
