7f9e028d909153ad97c7afd38fb3776b0d6ca5c2
saadahmadhere <saadahmad728@gmail.com>
2024-10-10 02:04:37 +0530

    feat: add common folder

---
+++ common/src/index.ts
@@ -0,0 +1,34 @@
+import { z } from 'zod';
+
+//user
+const signupInput = z.object({
+	email: z.string().email(),
+	firstName: z.string(),
+	lastName: z.string().optional(),
+	password: z.string().min(6),
+});
+
+const signinInput = z.object({
+	email: z.string().email(),
+	password: z.string(),
+});
+
+// blog
+const createBlogInput = z.object({
+	title: z.string(),
+	content: z.string(),
+	authorId: z.string(),
+});
+
+const updateBlogInput = z.object({
+	title: z.string(),
+	content: z.string(),
+	id: z.string(),
+});
+
+//user
+export type SignupInput = z.infer<typeof signupInput>;
+export type SigninInput = z.infer<typeof signinInput>;
+//blog
+export type CreateBlogInput = z.infer<typeof createBlogInput>;
+export type updateBlogInput = z.infer<typeof updateBlogInput>;
