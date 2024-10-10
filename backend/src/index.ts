768e0f0e99b33128378ef0f17c94668c6a6a116e
saadahmadhere <saadahmad728@gmail.com>
2024-10-10 23:32:03 +0530

    fix: remove unwanted routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,28 +13,4 @@ const app = new Hono<{
 app.route('/user', userRouter);
 app.route('/blog', blogRouter);
 
-app.use('/blog/*', async (c, next) => {
-	// get the header
-	const header = c.req.header('authorization') || '';
-
-	const token = header.split(' ')[1];
-
-	// verify the header
-	const decodedPayload = await verify(token, c.env.JWT_SECRET);
-
-	if (decodedPayload.id) {
-		// if the header is correct, proceed.
-		next();
-	} else {
-		// if not, return 403 status code.
-		c.status(403);
-		return c.json({ error: 'unauthorized' });
-	}
-});
-
-app.post('/blog', (c) => c.text('blog post /'));
-app.put('/blog', (c) => c.text('blog put /'));
-app.get('/blog/:id', (c) => c.text('blog id /'));
-app.get('/blog/bulk', (c) => c.text('blog get /'));
-
 export default app;
