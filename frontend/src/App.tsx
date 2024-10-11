38e8bee98c882e026f2567b8522be78b90b8d71d
saadahmadhere <saadahmad728@gmail.com>
2024-10-12 02:00:06 +0530

    feat: add routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+import Blog from './pages/Blog';
+
 function App() {
-	return <>Just an app</>;
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/signup' element={<Signup />} />
+					<Route path='/signin' element={<Signin />} />
+					<Route path='/blog/:id' element={<Blog />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
