6b5e858996e88ccfe5cbe827b423c68dbd012b5a
saadahmadhere <saadahmad728@gmail.com>
2024-10-17 11:32:14 +0530

    feat: add /blogs get api call

--- frontend/src/pages/BlogList.tsx
+++ frontend/src/pages/BlogList.tsx
@@ -1,5 +1,30 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { BACKEND_URL } from '../config';
+
 const BlogList = () => {
-	return <div>BlogList</div>;
+	const [blogs, setBlogs] = useState([]);
+	useEffect(() => {
+		(async () => {
+			const tokenStr = localStorage.getItem('token');
+			const res = await axios.get(`${BACKEND_URL}/blog/bulk`, {
+				headers: { Authorization: `${tokenStr}` },
+			});
+			if (res.status === 200) {
+				setBlogs(res.data.blogs);
+			}
+		})();
+	}, []);
+	if (blogs.length === 0) {
+		return <div>No blogs found</div>;
+	}
+	return (
+		<div>
+			{blogs.map((blog) => {
+				return <div key={blog}>{blog}</div>;
+			})}
+		</div>
+	);
 };
 
 export default BlogList;
