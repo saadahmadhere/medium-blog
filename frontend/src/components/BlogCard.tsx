6a8f56d916c084d27eac5937c05861608de01b31
saadahmadhere <saadahmad728@gmail.com>
2024-10-20 14:33:32 +0530

    feat: add blog card

---
+++ frontend/src/components/BlogCard.tsx
@@ -0,0 +1,45 @@
+import { useNavigate } from 'react-router-dom';
+
+interface BlogCardProps {
+	title: string;
+	content: string;
+	createdAt: string;
+	authorName: string;
+	blogId: string;
+}
+
+const BlogCard = ({
+	title,
+	content,
+	createdAt,
+	authorName,
+	blogId,
+}: BlogCardProps) => {
+	const navigate = useNavigate();
+
+	const handleBlogCardClick = (): void => {
+		navigate(`${blogId}`);
+	};
+	const getTruncatedContent = (content: string): string => {
+		if (content.length > 100) {
+			return content.substring(0, 100) + '...';
+		}
+		return content;
+	};
+	return (
+		<div
+			className='border-solid border-2 border-gray-400 rounded mb-4 cursor-pointer'
+			onClick={handleBlogCardClick}
+		>
+			<div>{title}</div>
+			<div>{getTruncatedContent(content)}</div>
+			<div>
+				created at:
+				{new Date(createdAt).toLocaleDateString()}
+			</div>
+			<div>Author: {authorName}</div>
+		</div>
+	);
+};
+
+export default BlogCard;
--- frontend/src/pages/BlogList.tsx
+++ frontend/src/pages/BlogList.tsx
@@ -1,27 +1,65 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { BACKEND_URL } from '../config';
+import BlogCard from '../components/BlogCard';
 
 const BlogList = () => {
-	const [blogs, setBlogs] = useState([]);
+	interface AuthorType {
+		firstName: string;
+		id: string;
+	}
+	interface BlogType {
+		title: string;
+		content: string;
+		isPublished: boolean;
+		id: string;
+		author: AuthorType;
+		createdAt: string;
+		updatedAt: string;
+	}
+	type blogListType = BlogType[];
+
+	const [blogs, setBlogs] = useState<blogListType>([]);
+	const [loading, setLoading] = useState<boolean>(false);
 	useEffect(() => {
 		(async () => {
-			const tokenStr = localStorage.getItem('token');
-			const res = await axios.get(`${BACKEND_URL}/blog/bulk`, {
-				headers: { Authorization: `${tokenStr}` },
-			});
-			if (res.status === 200) {
-				setBlogs(res.data.blogs);
+			try {
+				const tokenStr = localStorage.getItem('token');
+				setLoading(true);
+				const res = await axios.get(`${BACKEND_URL}/blog/bulk`, {
+					headers: { Authorization: `${tokenStr}` },
+				});
+
+				if (res.status === 200) {
+					setBlogs(res.data.blogs);
+				}
+			} catch (error) {
+				console.log('some error occuered: ', { error });
+			} finally {
+				setLoading(false);
 			}
 		})();
 	}, []);
+	if (loading) {
+		return <div>Loading...</div>;
+	}
 	if (blogs.length === 0) {
 		return <div>No blogs found</div>;
 	}
 	return (
-		<div>
+		<div className='flex flex-col items-center pt-4'>
 			{blogs.map((blog) => {
-				return <div key={blog}>{blog}</div>;
+				return (
+					<div key={blog.title} className='w-1/2'>
+						<BlogCard
+							title={blog.title}
+							content={blog.content}
+							createdAt={blog.createdAt}
+							authorName={blog.author.firstName}
+							blogId={blog.id}
+						/>
+					</div>
+				);
 			})}
 		</div>
 	);
