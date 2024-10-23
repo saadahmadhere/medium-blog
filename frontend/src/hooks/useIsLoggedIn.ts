eda751256ec691f2fa45652b18aa0ff53f65cfff
saadahmadhere <saadahmad728@gmail.com>
2024-10-23 23:27:15 +0530

    feat: token in isLoggedIn hook

--- frontend/src/hooks/useIsLoggedIn.ts
+++ frontend/src/hooks/useIsLoggedIn.ts
@@ -2,14 +2,16 @@ import { useEffect, useState } from 'react';
 
 const useIsLoggedIn = () => {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [token, setToken] = useState<string | undefined>(undefined);
 
 	useEffect(() => {
 		const tokenStr = localStorage.getItem('token');
 		if (tokenStr) {
+			setToken(tokenStr);
 			setIsLoggedIn(true);
 		}
 	}, []);
-	return { isLoggedIn };
+	return { isLoggedIn, token };
 };
 
 export default useIsLoggedIn;
